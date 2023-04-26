bf79c4bd3b60579ef763cca24cfe6dda29a30865
sriniciti19871 <131886712+sriniciti19871@users.noreply.github.com>
2023-04-26 08:59:56 -0500

    Add files via upload

---
+++ TwitterCurlCall/Models/SecretSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterCurlCall.Models
+{
+    public class SecretSettings
+    {
+        public string ConsumerKey { get; set; }
+        public string Authorization { get; set; }
+
+        public string APIendpoint { get; set; }
+        public string ConsumerSecret { get; set; }
+
+    }
+}
---
+++ TwitterCurlCall/Program.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using TwitterCurlCall.Models;
+
+
+
+
+//Try catch block for exception handling
+try
+{
+    
+
+    var url = "https://api.twitter.com/2/tweets/sample/stream";
+
+    //using block used to enforce use and dispose
+    using var client = new HttpClient();
+    {
+        //Twitter API has been successful at 32 mins in my previous executions. So specifying timeout as 60 minutes
+        client.Timeout = TimeSpan.FromHours(1);
+        var msg = new HttpRequestMessage(HttpMethod.Get, url);
+        msg.Headers.Add("Authorization", "Bearer AAAAAAAAAAAAAAAAAAAAAOKolQEAAAAAhG7h66BZfXp4R%2F6ZeawcUZRzRp0%3DtIHtrLDbV1vgQCYXtQLPoJ2BAgvB5pWK2NbAmHWWp6Mi6renDC");
+        msg.Headers.Add("ConsumerKey", "cC3D7bk5rT6xkFiQEVJLfMzpT");
+        msg.Headers.Add("ConsumerSecret", "LbBXZBp8OkzxfdLHnZtLEYBhmDptkZFTBLu87XYiT0ZqMyvfhs");
+
+        //Response from API
+        var res = await client.SendAsync(msg);
+
+      
+        
+       
+    var content = await res.Content.ReadAsStringAsync();
+
+       
+
+    }
+}
+//code specifically for a WebException
+catch (System.Net.WebException ex)
+{
+   
+        Console.WriteLine(ex.Message);
+    
+   
+}
+//Generic Exception block
+catch (Exception ex)
+{
+   
+    Console.WriteLine("Generic Exception occured when API was contacted" + ex.Message); 
+   
+}
+
+finally
+{
+    Console.WriteLine("Twitter API was attempted to connected");
+}
+
+
+
